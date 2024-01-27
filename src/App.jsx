983d08abf267909a4b27534d42ae8c451ba2056b
vandana <vsvankhede06@gmail.com>
2024-01-27 07:30:26 +0000

    complete file explorer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import "./styles.css";
 import explorer from "./data/FolderData";
 import { useState } from "react";
 import Folder from "./Components/Folder";
+import useTraverseTree from "./Hook/use-traverse-tree";
 
 export default function App() {
   const [explorerData, setExplorer] = useState(explorer);
+  const { insertNode } = useTraverseTree();
+  const handleInsertNode = (folderId, item, isFolder) => {
+    const finalTree = insertNode(explorerData, folderId, item, isFolder);
+    setExplorer(finalTree);
+  };
   return (
     <div className="App">
-      <Folder explorer={explorer} />
+      <Folder handleInsertNode={handleInsertNode} explorer={explorerData} />
     </div>
   );
 }
--- src/Components/Folder.jsx
+++ src/Components/Folder.jsx
@@ -1,17 +1,57 @@
 import { useState } from "react";
 
-function Folder({ explorer }) {
+function Folder({ handleInsertNode, explorer }) {
   const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    visible: false,
+    isFolder: null,
+  });
+  const handleNewFolder = (e, isFolder) => {
+    setExpand(true);
+    e.stopPropagation();
+    setShowInput({
+      visible: true,
+      isFolder,
+    });
+  };
+  const onAddFolder = (e) => {
+    if (e.keyCode === 13 && e.target.value) {
+      handleInsertNode(explorer.id, e.target.value, showInput.isFolder);
+      setShowInput({ ...showInput, visible: false });
+    }
+  };
   console.log(explorer);
   if (explorer.isFolder) {
     return (
       <div style={{ marginTop: 5 }}>
         <div className="folder" onClick={() => setExpand(!expand)}>
           <span>📁{explorer.name}</span>
+          <div>
+            <button onClick={(e) => handleNewFolder(e, true)}>Folder +</button>
+            <button onClick={(e) => handleNewFolder(e, false)}>File +</button>
+          </div>
         </div>
-        <div style={{ display: expand ? "block" : "none" }}>
+        <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
+          {showInput.visible && (
+            <div className="inputContainer">
+              <span>{showInput.isFolder ? "📁" : "📄"}</span>
+              <input
+                type="text"
+                onKeyDown={onAddFolder}
+                onBlur={() => setShowInput({ ...showInput, visible: false })}
+                className="inputContainer__input"
+                autoFocus
+              />
+            </div>
+          )}
           {explorer.items.map((exp) => {
-            return <span>{exp.name}</span>;
+            return (
+              <Folder
+                handleInsertNode={handleInsertNode}
+                explorer={exp}
+                key={exp.id}
+              />
+            );
           })}
         </div>
       </div>
